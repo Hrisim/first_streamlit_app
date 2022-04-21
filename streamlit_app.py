61e5a88c95ca9350eaf456ae6406a8e036e3f5f0
Hrisim <104139776+Hrisim@users.noreply.github.com>
2022-04-21 15:35:37 +0300

    Update streamlit_app.py
    change header 
    add emojies to the meals

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title("My parents healthy dinner")
 
-streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 & Blueberry otmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled free-range egg')
+streamlit.header('Breakfast favourite')
+streamlit.text('🥣 Omega 3 & Blueberry otmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text(🐔'Hard-Boiled free-range egg')
+streamlit.text('🥑🍞 Avocado Toast')
