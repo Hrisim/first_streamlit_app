aea052706d42444e94e31e2b8d6b8f521e07bc66
Hrisim <104139776+Hrisim@users.noreply.github.com>
2022-05-30 15:54:43 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("SELECT CURRENT_USER(hrisim), CURRENT_ACCOUNT(zg52538), CURRENT_REGION(xy12345.europe-west2.gcp)")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
