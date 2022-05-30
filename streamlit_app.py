f796e7949be438e76102f0526dcc267f1dc64d6b
Hrisim <104139776+Hrisim@users.noreply.github.com>
2022-05-30 16:41:31 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets[USER, PASSWORD, ACCOUNT, WAREHOUSE, DATABASE, SCHEMA ])
+my_cnx = snowflake.connector.connect(**streamlit.secrets[USER, PASSWORD, ACCOUNT, WAREHOUSE, DATABASE, SCHEMA])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(hrisim), CURRENT_ACCOUNT(zg52538), CURRENT_REGION(xy12345.europe-west2.gcp)")
 my_data_row = my_cur.fetchone()
